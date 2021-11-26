be6e8e3ad144801505a2f7bbd4ec82204738d690
mdashlw <mdashlw@gmail.com>
2021-11-26 09:18:15 +0300

    Use another canvas library; cleanup

--- example.js
+++ example.js
@@ -1,10 +1,11 @@
-import { Canvas } from "skia-canvas";
+import fs from "fs/promises";
+import Canvas from "canvas";
 import { drawMinecraftText } from "./lib/index.js";
 
-const canvas = new Canvas(150, 40);
+const canvas = Canvas.createCanvas(150, 40);
 const context = canvas.getContext("2d");
 
 drawMinecraftText(context, "§cHello world!", 0, 0, 2);
 drawMinecraftText(context, "§1C§2o§3l§4o§5r§6m§7a§8t§9i§ac", 0, 10, 2);
 
-canvas.saveAs("example.png");
+await fs.writeFile("example.png", canvas.toBuffer());
