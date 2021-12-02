655f802fd52886168a94f460bcb3be0f3239a8ad
mdashlw <mdashlw@gmail.com>
2021-12-02 07:51:03 +0300

    Support bold (§l) and reset (§r)

--- example.js
+++ example.js
@@ -2,10 +2,11 @@ import fs from "fs/promises";
 import Canvas from "canvas";
 import { drawMinecraftText } from "./lib/index.js";
 
-const canvas = Canvas.createCanvas(150, 40);
+const canvas = Canvas.createCanvas(150, 60);
 const context = canvas.getContext("2d");
 
 drawMinecraftText(context, "§cHello world!", 0, 0, 2);
 drawMinecraftText(context, "§1C§2o§3l§4o§5r§6m§7a§8t§9i§ac", 0, 10, 2);
+drawMinecraftText(context, "§6§lBold!", 0, 20, 2);
 
 await fs.writeFile("example.png", canvas.toBuffer());
